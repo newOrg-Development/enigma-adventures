844a520f6e73c357c4c4ae4c5d4aac00241be226
Jal <stew.pendousman4444@gmail.com>
2023-02-28 00:08:11 -0800

    added build pack for googleCreds.json

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 const mainHead = module.require("./views/custom/mainHead.hbs");
 const leaderboardHead = module.require("./views/custom/leaderboardHead.hbs");
 const resetHead = module.require("./views/custom/resetHead.hbs");
+const adminHead = module.require("./views/custom/adminHead.hbs");
 
 const emailRouter = require("./routes/email");
 const homeRouter = require("./routes/homeRouter");
