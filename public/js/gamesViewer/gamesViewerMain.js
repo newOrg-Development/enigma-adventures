87027a32b6f900ec882b379eeb8013a022bcf5b6
Jal <stew.pendousman4444@gmail.com>
2023-03-02 04:50:45 -0800

    game Viewer updates

--- public/js/gamesViewer/gamesViewerMain.js
+++ public/js/gamesViewer/gamesViewerMain.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
   tHead.rows[0].innerHTML = "";
   table.getElementsByTagName("tbody")[0].innerHTML = "";
   for (let i = 0; i < gamesHeaders.length; i++) {
+    if (i == 0 || i == 1 || i == 3) continue;
     let th = document.createElement("th");
     th.setAttribute("scope", "col");
     th.innerHTML = gamesHeaders[i];
@@ -29,17 +30,36 @@ $(document).ready(function () {
 
   gameStatesDataArray.forEach((gameState, index) => {
     let tr = document.createElement("tr");
-    let th = document.createElement("th");
-    th.setAttribute("scope", "row");
-    th.innerHTML = index + 1;
-    tr.appendChild(th);
+    // let th = document.createElement("th");
+    // th.setAttribute("scope", "row");
+    // th.innerHTML = index + 1;
+    //tr.appendChild(th);
     for (let i = 0; i < gameState.length; i++) {
+      if (i == 0 || i == 2) continue; // skip the first element (gameId
       let td = document.createElement("td");
       if (gameState[i] == gameState[4]) {
         let date = new Date(parseInt(gameState[i]));
         let dateStr = date.toDateString();
-        let timeStr = date.toLocaleTimeString();
-        let dateTimeStr = dateStr + " " + timeStr;
+        function formatDate(date) {
+          var d = new Date(date),
+            month = "" + (d.getMonth() + 1),
+            day = "" + d.getDate(),
+            year = d.getFullYear();
+
+          if (month.length < 2) month = "0" + month;
+          if (day.length < 2) day = "0" + day;
+
+          return [year, month, day].join("/");
+        }
+        let dateStrParsed = formatDate(dateStr);
+
+        let timeStr = date.toLocaleTimeString(navigator.language, {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        });
+
+        let dateTimeStr = dateStrParsed + " " + timeStr;
         td.innerHTML = dateTimeStr;
       } else {
         td.innerHTML = gameState[i];
